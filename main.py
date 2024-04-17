eba5572b972d4d85e61c2df4140c1d18a625416f
Daloshka <andrey_balakirevv@mail.ru>
2024-04-18 02:29:38 +0800

    Updated descriptions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 
-from config import API_TOKEN
+# СОЗДАЙТЕ ФАЙЛ config.py И ПОМЕСТИТЕ В НЕГО СВОЙ API ТОКЕН с сайта https://goo.su/zJLQac5
+from config import API_TOKEN 
 
-# API endpoint
+
 
 class SMMNice():
     def __init__(self):
@@ -16,13 +17,21 @@ class SMMNice():
         else:
             return url
 
-    def get_services(self):
+    def get_services(self) -> dict:
+        """
+        Получение списка услуг в JSON
+        """
         url = f"https://smmnice.ru/api/v2?action=services&key={self.token}"
         response = requests.get(url)
         data = response.json()
         return data
 
-    def order_views(self, url):
+    def order_post_views(self, url:str) -> dict:
+        """
+        params: url - ссылка на пост
+        return: dict - 1/0 (успешно/неуспешно)
+        Получение информации о заказе просмотров
+        """
         order_url = self.cut_https(url)
         url = f"https://smmnice.ru/api/v2?action=add&service=2341&link={order_url}&quantity=100&key={self.token}"
         response = requests.get(url)
@@ -33,3 +42,5 @@ class SMMNice():
 if __name__ == "__main__":
     smm = SMMNice()
     
+    # TEST
+    #print(smm.order_views(order_url=""))
